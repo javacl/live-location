36d62e1b232bd469a412bff830409f59a638b430
sadeghi <javacl.sadeghi@gmail.com>
2022-10-17 12:43:14 +0330

    live-location-in-background

--- app/src/main/java/com/live/location/main/MainScreen.kt
+++ app/src/main/java/com/live/location/main/MainScreen.kt
@@ -1,13 +1,23 @@
 package com.live.location.main
 
 import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -17,6 +27,9 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 import com.live.location.service.LiveLocationService
 
 @ExperimentalPermissionsApi
@@ -24,6 +37,10 @@ import com.live.location.service.LiveLocationService
 fun MainScreen() {
 
     val context = LocalContext.current
+
+    var needCheckLocationSetting by rememberSaveable { mutableStateOf(true) }
+
+    var locationSettingIsEnable by rememberSaveable { mutableStateOf(false) }
 
     val locationPermissionState = rememberMultiplePermissionsState(
         listOf(
@@ -36,6 +53,26 @@ fun MainScreen() {
         Manifest.permission.ACCESS_BACKGROUND_LOCATION
     )
 
+    val settingResultRequest = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartIntentSenderForResult()
+    ) { activityResult ->
+        if (activityResult.resultCode == RESULT_OK) {
+            needCheckLocationSetting = true
+        }
+    }
+
+    if (needCheckLocationSetting) {
+        checkLocationSetting(
+            context = context,
+            onDisabled = { intentSenderRequest ->
+                settingResultRequest.launch(intentSenderRequest)
+            },
+            onEnabled = { locationSettingIsEnable = true }
+        )
+        needCheckLocationSetting = false
+    }
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,7 +82,10 @@ fun MainScreen() {
         verticalArrangement = Arrangement.Center
     ) {
 
-        if (locationPermissionState.allPermissionsGranted && backgroundPermissionState.status.isGranted) {
+        if (locationPermissionState.allPermissionsGranted
+            && backgroundPermissionState.status.isGranted
+            && locationSettingIsEnable
+        ) {
 
             Text(
                 text = "🌍 Location Tracker Background Service.!!",
@@ -85,16 +125,52 @@ fun MainScreen() {
             }
         } else {
             Button(onClick = {
-                if (locationPermissionState.allPermissionsGranted) {
-                    backgroundPermissionState.launchPermissionRequest()
+                if (locationSettingIsEnable) {
+                    if (locationPermissionState.allPermissionsGranted) {
+                        backgroundPermissionState.launchPermissionRequest()
+                    } else {
+                        locationPermissionState.launchMultiplePermissionRequest()
+                    }
                 } else {
-                    locationPermissionState.launchMultiplePermissionRequest()
+                    needCheckLocationSetting = true
                 }
             }) {
                 Text(
                     fontSize = MaterialTheme.typography.subtitle1.fontSize,
                     text = "Grant Permission"
                 )
+            }
+        }
+    }
+}
+
+fun checkLocationSetting(
+    context: Context,
+    onDisabled: (IntentSenderRequest) -> Unit,
+    onEnabled: () -> Unit
+) {
+    val locationRequest = LocationRequest.create().apply {
+        interval = 1000
+        fastestInterval = 1000
+    }
+
+    val client: SettingsClient = LocationServices.getSettingsClient(context)
+    val builder: LocationSettingsRequest.Builder = LocationSettingsRequest
+        .Builder()
+        .addLocationRequest(locationRequest)
+
+    val gpsSettingTask: Task<LocationSettingsResponse> =
+        client.checkLocationSettings(builder.build())
+
+    gpsSettingTask.addOnSuccessListener { onEnabled() }
+    gpsSettingTask.addOnFailureListener { exception ->
+        if (exception is ResolvableApiException) {
+            try {
+                val intentSenderRequest = IntentSenderRequest
+                    .Builder(exception.resolution)
+                    .build()
+                onDisabled(intentSenderRequest)
+            } catch (sendEx: IntentSender.SendIntentException) {
             }
         }
     }
